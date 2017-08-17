24cf2fa68193ee82ea00a1e49ce924d04d2474fb
Fernando Monllor <fernando.monllor@sumamoos.com>
2017-08-17 12:41:42 +0200

    Prueba de envío de emails.

--- controllers/siloe_controller.js
+++ controllers/siloe_controller.js
@@ -141,7 +141,7 @@ console.log('2');
 
 					// create reusable transporter object using the default SMTP transport
 					let transporter = nodemailer.createTransport({
-						host: 'registrosdemantenimiento.com',
+						host: 'smtp.registrosdemantenimiento.com',
 						port: 465,
 						secure: true, // secure:true for port 465, secure:false for port 587
 						auth: {
