19b5cb2766489ba78d3766a1ec36d9b3a136bf2b
Fernando Monllor <fernando.monllor@sumamoos.com>
2017-08-19 10:58:24 +0200

    no message

--- controllers/siloe_controller.js
+++ controllers/siloe_controller.js
@@ -919,14 +919,13 @@
 					let transporter = nodemailer.createTransport({				// create reusable transporter object using the default SMTP transport
 						host: 'registrosdemantenimiento.com',
 						port: 465,
-						secure: true, 											// secure:true for port 465, secure:false for port 587
+						secure: true, // secure:true for port 465, secure:false for port 587
 						auth: {
-							user: 'noreply@registrosdemantenimiento.com',
-							pass: process.env.NODE_SMTP_PASS
+							user: 'contacto@registrosdemantenimiento.com',
+							pass: 'j@3eC05o'
 						},
 						tls: {
-					        rejectUnauthorized: false							// do not fail on invalid certs
-					    }
+					        rejectUnauthorized: false	// do not fail on invalid certs
 					});
 
 					let mailOptions = {																// setup email data with unicode symbols
