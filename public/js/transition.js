f00b8398d123a2b7a572e2bb182bc4a2ebc68402
jotamontoyo <jotamontoyo@gmail.com>
2017-10-04 19:51:47 +0100

    boton subir

--- public/js/transition.js
+++ public/js/transition.js
@@ -11,15 +11,32 @@
 		anim();
 
 
-        $('#subir-inicio-icon').css('opacity', '0.8');                                  // control de opacity/visibility del icono subir
-        clearTimeout($.data(this, 'scrollTimer'));
-        $.data(this, 'scrollTimer', setTimeout(function() {
-            $('#subir-inicio-icon').css('opacity', '0.3');
-        }, 250));
-        if ($(window).scrollTop() === 0) {
-            $('#subir-inicio').css('visibility', 'hidden');
-        } else {
-            $('#subir-inicio').css('visibility', 'visible');
-        };
+
+        // Page Scroll Control
+        var nav = $('nav[role="navigation"]');
+
+        nav.find('a').on('click', function() {
+            var $el = $(this),                                      // selecciona el elemento que ha disparado la funcion
+                id = $el.attr('href');                              // captura el href del elemento
+            $('html, body').animate({                               // aplica un animate scrollTop al href
+                scrollTop: $(id).offset().top
+            }, 800);
+            $('#subir-inicio a').css('background-color', 'transparent');
+            return false;
+        });
+
+        $(window).scroll(function() {
+            $('#subir-inicio-icon').css('opacity', '0.8');                                  // control de opacity/visibility del icono subir
+            clearTimeout($.data(this, 'scrollTimer'));
+            $.data(this, 'scrollTimer', setTimeout(function() {
+                $('#subir-inicio-icon').css('opacity', '0.3');
+            }, 250));
+            if ($(window).scrollTop() === 0) {
+                $('#subir-inicio').css('visibility', 'hidden');
+                $('#subir-inicio-icon').css('background-color', 'transparent');
+            } else {
+                $('#subir-inicio').css('visibility', 'visible');
+            };
+        });
 
     });
